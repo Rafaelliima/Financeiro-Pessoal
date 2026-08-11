e42a524b88dbc17c0ead0aac3602330c70c531f6
Rafaelliima <130843181+Rafaelliima@users.noreply.github.com>
2026-08-11 12:34:22 -0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
     applicationId = "com.aistudio.financeiro.pessoal.app"
     minSdk = 24
     targetSdk = 36
-    versionCode = 2
-    versionName = "1.1-Teste"
+    versionCode = 3
+    versionName = "1.2"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
